2d45c989f1b4c33279a9973f7df26e23fe3e1091
Ramon <ramon.fernando98@gmail.com>
2025-05-29 17:30:15 -0300

    pokemon and type entities modified and configured test environment

--- src/main/java/com/ramon/pokedex/entities/Pokemon.java
+++ src/main/java/com/ramon/pokedex/entities/Pokemon.java
@@ -5,6 +5,7 @@ import jakarta.persistence.*;
 import java.util.Objects;
 
 @Entity
+@Table(name = "tb_pokemon")
 public class Pokemon {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
